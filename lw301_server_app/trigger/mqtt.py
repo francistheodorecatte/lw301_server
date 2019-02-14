0901458213c3259e3ca4c7c1e8d83f05f97d229c
Yury Skaletskiy <yury.skaletskiy@gmail.com>
2019-02-14 19:56:57 +0000

    added channel to mqtt topic

--- lw301_server_app/trigger/mqtt.py
+++ lw301_server_app/trigger/mqtt.py
@@ -51,7 +51,7 @@ class MqttTrigger(Trigger):
     async def on_new_data(self, measurement, value):
         v = value._asdict()
 
-        self.client.publish("lw301/{}/{}".format(v['mac'], measurement), json.dumps(v))
+        self.client.publish("lw301/{}_{}/{}".format(v['mac'], v['channel'], measurement), json.dumps(v))
 
     #     # delay processing for next ioloop tick
     #     self.ioloop.add_callback(
